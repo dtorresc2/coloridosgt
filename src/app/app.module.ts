2b7c5e9a1d8810a8b5c3cb2249babb5983ebe562
dtorresc2 <dtorresc2@miumg.edu.gt>
2020-08-09 16:47:11 -0600

    Agregnado validaciones a registro del cliente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
@@ -26,7 +26,8 @@ import { SinginComponent } from './routes/singin/singin.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/routes/singup/singup.component.ts
+++ src/app/routes/singup/singup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, ValidatorFn, ValidationErrors } from '@angular/forms';
+import { Variable } from '@angular/compiler/src/render3/r3_ast';
 
 @Component({
   selector: 'app-singup',
@@ -6,10 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./singup.component.css']
 })
 export class SingupComponent implements OnInit {
+  user: FormGroup
 
-  constructor() { }
+  constructor() {
+
+  }
 
   ngOnInit(): void {
+    this.user = new FormGroup(
+      {
+        email: new FormControl('', [Validators.required, Validators.email]),
+        name: new FormControl('', Validators.required),
+        lastname: new FormControl('', Validators.required),
+        password: new FormControl('', Validators.required),
+        confirmpass: new FormControl('', Validators.required)
+      },
+      {
+        validators: this.passwordMatchValidator
+      }
+    );
+
   }
+
+  onSubmit() {
+
+  }
+
+  // Funcion de confirmacion de usuarios
+  passwordMatchValidator(g: FormGroup): { invalid: boolean } {
+    return g.get('password').value === g.get('confirmpass').value ? null : { invalid: true };
+  }
+
 
 }
