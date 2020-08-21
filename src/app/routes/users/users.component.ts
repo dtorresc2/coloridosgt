be1f67d86ad88805ac2f7b9d46c0862c168bcb85
dtorresc2 <dtorresc2@miumg.edu.gt>
2020-08-20 20:20:15 -0600

    Ajustando form users - validaciones de ingreso

--- src/app/routes/users/users.component.ts
+++ src/app/routes/users/users.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from 'src/app/services/usuarios/users.service';
+import { Router } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Usuario } from 'src/app/controllers/usuario';
 
 @Component({
   selector: 'app-users',
@@ -6,10 +10,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  idUsuario: any;
+  user: FormGroup;
 
-  constructor() { }
+  usuario: Usuario = {
+    email: '',
+    user: 'user',
+    password: ''
+  }
+
+  constructor(private usersService: UsersService, private router: Router) { }
 
   ngOnInit(): void {
+
+    this.user = new FormGroup(
+      {
+        email: new FormControl('', [Validators.required, Validators.email]),
+        username: new FormControl('', Validators.required),
+        password: new FormControl('', Validators.required)
+      }
+    );
+    
+    this.idUsuario = localStorage.getItem('idUsuario');
+
+    if (this.idUsuario > 0) {
+      this.router.navigate(['/home']);
+    }
+    else {
+      localStorage.clear();
+      this.usersService.autenticado = false;
+
+    }
+  }
+
+  onSubmit() {
+    // this.comprobador = true;
+    // this.autenticarUsuario();
   }
 
 }
