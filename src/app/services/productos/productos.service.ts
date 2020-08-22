1f33ce6daa68898f5dc3de8bf32ae3885c662e3e
dtorresc2 <dtorresc2@miumg.edu.gt>
2020-08-21 19:54:02 -0600

    Implementando catalogo de productos

---
+++ src/app/services/productos/productos.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Servidor } from 'src/app/config/config';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductosService {
+
+  constructor(private http: HttpClient) { }
+
+  obtenerProductos() {
+    return this.http.get(Servidor.API_URI + '/productos');
+  }
+
+}
