c0243ade520a937108df2b62831ab07cd2734c83
dtorresc2 <dtorresc2@miumg.edu.gt>
2020-08-26 22:20:42 -0600

    Filtro de productos completado

--- src/app/pipes/productos/filtro-producto.pipe.ts
+++ src/app/pipes/productos/filtro-producto.pipe.ts
@@ -5,8 +5,16 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FiltroProductoPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  // transform(value: unknown, ...args: unknown[]): unknown {
+  transform(value: any, args: any): any {
+    const resultadoProductos = [];
+    
+    for (const producto of value) {
+      if (producto.nombre.toLowerCase().indexOf(args.toLowerCase()) > -1 || producto.descripcion.toLowerCase().indexOf(args.toLowerCase()) > -1 ){
+        resultadoProductos.push(producto);
+      };
+    };
+    return resultadoProductos;
   }
 
 }
