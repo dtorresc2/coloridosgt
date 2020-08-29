33c64c8961d9ec258163bbb0f5eaa1b690cef6e5
dtorresc2 <dtorresc2@miumg.edu.gt>
2020-08-28 19:43:00 -0600

    Obteniendo listado de empleados - orders

--- src/app/routes/orders/orders.component.ts
+++ src/app/routes/orders/orders.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { UsersService } from 'src/app/services/usuarios/users.service';
+import { Router } from '@angular/router';
+import { CategoriasService } from 'src/app/services/productos/categorias.service';
 
 
 @Component({
@@ -8,15 +11,45 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./orders.component.css']
 })
 export class OrdersComponent implements OnInit {
+  ID: any;
+  idUsuario: any;
 
-  constructor(private modalService: NgbModal) { }
+  listaEmpleados: any = [];
+
+  constructor(
+    private modalService: NgbModal, 
+    private usersService: UsersService,
+    private categoriaService: CategoriasService, 
+    private router: Router) { }
+
+  ngOnInit(): void {
+    this.idUsuario = localStorage.getItem('idUsuario');
+
+    if (this.idUsuario > 0) {
+      this.ID = 'Registrado';
+    }
+    else {
+      this.ID = 'Inicie Sesion';
+      this.router.navigate(['/singin']);
+    }
+
+    this.obtenerListaEmpleados();
+  }
 
   openScrollableContent(longContent, id) {
     this.modalService.open(longContent, { scrollable: true, size: 'lg', centered: true });
     console.log(id);
   }
 
-  ngOnInit(): void {
+  obtenerListaEmpleados() {
+    this.usersService.obtenerUsuarios()
+      .subscribe(
+        res => {
+          this.listaEmpleados = res;
+          // console.log(res);
+        },
+        err => console.error(err)
+      )
   }
 
 }
