78ccf4fd4c55b8f0a54f63b15e0ab1f86736a1c6
dtorresc2 <dtorresc2@miumg.edu.gt>
2020-09-01 22:01:14 -0600

    Ajustando modulo pedidos - 8.3

--- src/app/pipes/pedidos/filtro-pedidos.pipe.ts
+++ src/app/pipes/pedidos/filtro-pedidos.pipe.ts
@@ -5,8 +5,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FiltroPedidosPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any, args: any): any {
+    const resultadoPedidos = [];
+    
+    for (const pedido of value) {
+      if (pedido.cliente.toLowerCase().indexOf(args.toLowerCase()) > -1 || pedido.direccion.toLowerCase().indexOf(args.toLowerCase()) > -1 ){
+        resultadoPedidos.push(pedido);
+      };
+    };
+    return resultadoPedidos;
   }
 
 }
