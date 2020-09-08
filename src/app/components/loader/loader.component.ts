ba022011f33cdeae5a1d0f93464e742f99c94a50
dtorresc2 <dtorresc2@miumg.edu.gt>
2020-09-07 20:21:02 -0600

    Implementando un loader en el catalogo 2

--- src/app/components/loader/loader.component.ts
+++ src/app/components/loader/loader.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationStart } from '@angular/router';
 
 @Component({
   selector: 'app-loader',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.router.events.subscribe(events => {
+      if (event instanceof NavigationStart){
+
+      }
+    })
   }
+
+  show = false;
 
 }
