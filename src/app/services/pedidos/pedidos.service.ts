4fb22206c4b26f01c43f132594e36e11d7d67d73
dtorresc2 <dtorresc2@miumg.edu.gt>
2020-09-09 14:08:03 -0600

    Ajustes en catalogo de productos 4.1

--- src/app/services/pedidos/pedidos.service.ts
+++ src/app/services/pedidos/pedidos.service.ts
@@ -6,29 +6,51 @@ import { DetallePedido, Pedido } from '../../controllers/pedido';
 })
 export class PedidosService {
   fieldArray: Array<DetallePedido> = [];
-  newAttribute: DetallePedido = {};
+  newAttribute: DetallePedido;
   pedido: Pedido;
   cantidadItems: any = 0;
 
   constructor() { }
 
   agregarPedido(detallePedido: DetallePedido) {
-    //console.log("Envie un pedido")
-    // this.newAttribute = {
-    //   desc : descripcion,
-    //   subtot : subtotal
-    // };
- 
-    console.log('Servicio ', detallePedido);
+    let filaNueva: DetallePedido = {
+      idProducto: detallePedido.idProducto,
+      cantidad: detallePedido.cantidad,
+      precio_unidad: detallePedido.precio_unidad,
+      subtotal: detallePedido.subtotal,
+      desc: detallePedido.desc,
+      producto: detallePedido.producto
+    };
 
-    if (this.fieldArray.findIndex(x => x.idProducto === detallePedido.idProducto) != -1){
-      let index = this.fieldArray.findIndex(x => x.idProducto === detallePedido.idProducto);
-      console.log(index);
+    let filaAuxiliar: DetallePedido = {
+      idProducto: 0,
+      cantidad: 0,
+      precio_unidad: 0.00,
+      subtotal: 0.00,
+      desc: '',
+      producto: ''
     }
 
+    // Funcion para detectar productos repetidos y actualizar la cantidad con el arreglo existente
+    if (this.fieldArray.findIndex(x => x.idProducto === detallePedido.idProducto) != -1) {
+      let index = this.fieldArray.findIndex(x => x.idProducto === detallePedido.idProducto);
 
-    this.fieldArray.push(detallePedido);
+      filaAuxiliar.idProducto = this.fieldArray[index].idProducto;
+      let cantidadNueva = (this.fieldArray[index].cantidad + detallePedido.cantidad);
+      filaAuxiliar.cantidad = cantidadNueva;
+      filaAuxiliar.precio_unidad = this.fieldArray[index].precio_unidad;
+      filaAuxiliar.subtotal = (cantidadNueva * this.fieldArray[index].precio_unidad);
+      filaAuxiliar.desc = this.fieldArray[index].desc;
+      filaAuxiliar.producto = this.fieldArray[index].producto;
+
+      console.log(this.fieldArray[index]);
+      console.log(filaAuxiliar);
+    }
+
+    // this.fieldArray.push
+    this.fieldArray.push(filaNueva);
     this.cantidadItems = this.fieldArray.length;
+    console.log(this.fieldArray);
   }
 
   crearPedidoGeneral() {
