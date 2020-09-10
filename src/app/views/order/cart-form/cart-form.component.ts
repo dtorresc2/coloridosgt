7c9158570c297fbd7838cb64c74cae10b660db08
dtorresc2 <dtorresc2@miumg.edu.gt>
2020-09-10 00:34:58 -0600

    Ajustando CheckOut 7

--- src/app/views/order/cart-form/cart-form.component.ts
+++ src/app/views/order/cart-form/cart-form.component.ts
@@ -4,6 +4,9 @@ import { Router } from '@angular/router';
 import { ClientsService } from 'src/app/services/clientes/clients.service';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
+import * as moment from 'moment';
+import 'moment-timezone';
+
 @Component({
   selector: 'app-cart-form',
   templateUrl: './cart-form.component.html',
@@ -11,6 +14,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class CartFormComponent implements OnInit {
   client: FormGroup;
+  fecha: any;
 
   constructor(private pedidoService: PedidosService, private router: Router, private clienteService: ClientsService) { }
 
@@ -20,6 +24,7 @@ export class CartFormComponent implements OnInit {
         .subscribe(
           res => {
             console.log(res);
+            this.cargarCliente(res);
           },
           err => console.error(err)
         );
@@ -32,8 +37,8 @@ export class CartFormComponent implements OnInit {
       {
         nombre: new FormControl('', [Validators.required, Validators.pattern('^[A-Z]{1}[a-z]+([" "]{0,1}[A-Z]{1}[a-z]+){0,1}$')]),
         apellido: new FormControl('', [Validators.required, Validators.pattern('^[A-Z]{1}[a-z]+([" "]{0,1}[A-Z]{1}[a-z]+){0,1}$')]),
-        nit: new FormControl('', [Validators.required, Validators.maxLength(10)]),
-        dpi: new FormControl('', [Validators.required, Validators.maxLength(15)]),
+        nit: new FormControl('', [Validators.required]),
+        dpi: new FormControl('', [Validators.required]),
         // telefono: new FormControl('', [Validators.required, Validators.pattern('^[2-8]{1}[0-9]{3}[-][0-9]{4}$')]),
         telefono: new FormControl('', [Validators.required, Validators.pattern('^[2-8]{1}[0-9]{3}[-][0-9]{4}$')]),
         direccion: new FormControl('', [Validators.required, Validators.maxLength(100)]),
@@ -44,6 +49,18 @@ export class CartFormComponent implements OnInit {
       }
     );
     // https://www.regextester.com/97987 TESTER DE EXPRESIONES REGULARES
+
+    this.fecha = moment().tz("America/Guatemala").format('DD/MM/YYYY');
+  }
+
+  cargarCliente(cliente) {
+    this.client.get('nombre').setValue((<any>cliente).nombre);
+    this.client.get('apellido').setValue((<any>cliente).apellido);
+    this.client.get('nit').setValue((<any>cliente).nit);
+    this.client.get('dpi').setValue((<any>cliente).dpi);
+    this.client.get('telefono').setValue((<any>cliente).telefono);
+    this.client.get('correo').setValue((<any>cliente).correo);
+    this.client.get('fecha').setValue(moment().tz("America/Guatemala").format('DD/MM/YYYY'));
   }
 
   onSubmit(){
@@ -54,3 +71,19 @@ export class CartFormComponent implements OnInit {
 // this.firstName.valueChanges.subscribe(value => {
 //   console.log('name has changed:', value)
 // });
+
+// apellido: "Claros"
+// ​
+// correo: "correo1@correo.com"
+// ​
+// dpi: null
+// ​
+// idcliente: 2
+// ​
+// nick: "correo1"
+// ​
+// nit: "CF"
+// ​
+// nombre: "Roberto"
+// ​
+// telefono: "0000-0000"
