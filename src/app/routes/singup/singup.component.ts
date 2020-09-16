72cf1b4cb3fa786e21a4ef0a86138ff9faa263d5
dtorresc2 <dtorresc2@miumg.edu.gt>
2020-09-15 21:01:31 -0600

    singup - validacion de nit

--- src/app/routes/singup/singup.component.ts
+++ src/app/routes/singup/singup.component.ts
@@ -44,7 +44,7 @@ export class SingupComponent implements OnInit {
         dpi: new FormControl('', Validators.required)
       },
       {
-        validators: this.passwordMatchValidator
+        validators: [this.passwordMatchValidator, this.nitMatchValidator]
       }
     );
 
@@ -66,6 +66,57 @@ export class SingupComponent implements OnInit {
   // Funcion de confirmacion de usuarios
   passwordMatchValidator(g: FormGroup): { invalid: boolean } {
     return g.get('password').value === g.get('confirmpass').value ? null : { invalid: true };
+  }
+
+  nitMatchValidator(g: FormGroup): { nitInvalido: boolean } {
+    let txtN = g.get('nit').value;
+    // console.log(txtN);
+
+    if (txtN != '') {
+      txtN = txtN.toUpperCase();
+      if (txtN == "CF" || txtN == "C/F") return null;
+      var nit = txtN;
+      var pos = nit.indexOf("-");
+
+      if (pos < 0) {
+        var correlativo = txtN.substr(0, txtN.length - 1);
+        correlativo = correlativo + "-";
+
+        var pos2 = correlativo.length - 2;
+        var digito = txtN.substr(pos2 + 1);
+        nit = correlativo + digito;
+        pos = nit.indexOf("-");
+        txtN = nit;
+      }
+
+      var Correlativo = nit.substr(0, pos);
+      var DigitoVerificador = nit.substr(pos + 1);
+      var Factor = Correlativo.length + 1;
+      var Suma = 0;
+      var Valor = 0;
+
+      for (let x = 0; x <= (pos - 1); x++) {
+        Valor = eval(nit.substr(x, 1));
+        // var Multiplicacion = eval(Valor * Factor);
+        var Multiplicacion = Valor * Factor;
+        // Suma = eval(Suma + Multiplicacion);
+        Suma = Suma + Multiplicacion;
+        Factor = Factor - 1;
+      }
+
+      var xMOd11 = 0;
+      xMOd11 = (11 - (Suma % 11)) % 11;
+      var s = xMOd11;
+
+      if ((xMOd11 == 10 && DigitoVerificador == "K") || (s == DigitoVerificador)) {
+        return null;
+      }
+      else {
+        return { nitInvalido: true };
+      }
+    }
+    // return { nitInvalido: true }
+    // return g.get('password').value === g.get('confirmpass').value ? null : { nitInvalido: true };
   }
 
   // Registrar Cliente
