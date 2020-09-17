ca15cbab4452388feadb2a2d249e4492da10a2df
dtorresc2 <dtorresc2@miumg.edu.gt>
2020-09-16 23:43:02 -0600

    Diseñando - modulo de finanzas

--- src/app/routes/accounts/accounts.component.ts
+++ src/app/routes/accounts/accounts.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from 'src/app/services/usuarios/users.service';
 import { Router } from '@angular/router';
+import { NgbCalendar, NgbDate, NgbDateParserFormatter } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-accounts',
@@ -11,7 +12,19 @@ export class AccountsComponent implements OnInit {
   ID: any;
   idUsuario: any;
 
-  constructor(private usersService: UsersService, private router: Router) { }
+  hoveredDate: NgbDate | null = null;
+  fromDate: NgbDate | null;
+  toDate: NgbDate | null;
+
+  constructor(
+    private usersService: UsersService,
+    private router: Router,
+    private calendar: NgbCalendar,
+    public formatter: NgbDateParserFormatter
+  ) {
+    this.fromDate = calendar.getToday();
+    this.toDate = calendar.getNext(calendar.getToday(), 'd', 10);
+  }
 
   ngOnInit(): void {
     this.idUsuario = localStorage.getItem('idUsuario');
@@ -23,6 +36,34 @@ export class AccountsComponent implements OnInit {
       this.ID = 'Inicie Sesion';
       this.router.navigate(['/singin']);
     }
+  }
+
+  onDateSelection(date: NgbDate) {
+    if (!this.fromDate && !this.toDate) {
+      this.fromDate = date;
+    } else if (this.fromDate && !this.toDate && date && date.after(this.fromDate)) {
+      this.toDate = date;
+    } else {
+      this.toDate = null;
+      this.fromDate = date;
+    }
+  }
+
+  isHovered(date: NgbDate) {
+    return this.fromDate && !this.toDate && this.hoveredDate && date.after(this.fromDate) && date.before(this.hoveredDate);
+  }
+
+  isInside(date: NgbDate) {
+    return this.toDate && date.after(this.fromDate) && date.before(this.toDate);
+  }
+
+  isRange(date: NgbDate) {
+    return date.equals(this.fromDate) || (this.toDate && date.equals(this.toDate)) || this.isInside(date) || this.isHovered(date);
+  }
+
+  validateInput(currentValue: NgbDate | null, input: string): NgbDate | null {
+    const parsed = this.formatter.parse(input);
+    return parsed && this.calendar.isValid(NgbDate.from(parsed)) ? NgbDate.from(parsed) : currentValue;
   }
 
 }
