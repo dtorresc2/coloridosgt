abcb058346598dd667ef4aff7c43823491420cc3
dtorresc2 <dtorresc2@miumg.edu.gt>
2020-09-25 22:20:00 -0600

    Agregando dashboard

--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -11,6 +11,13 @@ export class HomeComponent implements OnInit {
   ID: any;
   idUsuario: any;
 
+  arregloUsuario:any = [];
+  usuario:any;
+  correo:any;
+  permisos1:any;
+  permisos2:any;
+  permisos3:any;
+
   constructor(private usersService: UsersService, private router: Router) { }
 
   ngOnInit(): void {
@@ -23,6 +30,24 @@ export class HomeComponent implements OnInit {
       this.ID = 'Inicie Sesion';
       this.router.navigate(['/singin']);
     }
+
+    this.obtenerUsuario();
+  }
+
+  obtenerUsuario() {
+    this.usersService.obtenerUsuarios()
+      .subscribe(
+        res => {
+          // localStorage.setItem('userName', (<any>res).nick);
+          console.log(res);
+          this.arregloUsuario = res;
+          let encontrado = this.arregloUsuario.find(element => element.idusuario = this.idUsuario);
+          // console.log(encontrado.correo);
+          this.usuario = encontrado.nombrerol;
+          this.correo = encontrado.correo;
+        },
+        err => console.error(err)
+      );
   }
 
 }
