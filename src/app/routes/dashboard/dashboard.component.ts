0da69f58c3049fd2fa8c6f1de745ad3a621e1f3c
dtorresc2 <dtorresc2@miumg.edu.gt>
2020-09-25 21:16:09 -0600

    Agregando dashboard 1

--- src/app/routes/dashboard/dashboard.component.ts
+++ src/app/routes/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ClientsService } from 'src/app/services/clientes/clients.service';
 import { Router } from '@angular/router';
+import { PedidosService } from 'src/app/services/pedidos/pedidos.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,16 +11,33 @@ import { Router } from '@angular/router';
 export class DashboardComponent implements OnInit {
   ID: any;
   idUsuario: any;
+  listaPedidos: any = [];
+  total: any = 0;
 
-  constructor(private clientService: ClientsService, private router: Router) { }
+  constructor(
+    private clientService: ClientsService,
+    private router: Router,
+    private pedidoService: PedidosService
+  ) { }
 
   ngOnInit(): void {
 
     if (localStorage["idUsuario"]) {
       this.idUsuario = localStorage.getItem('idUsuario');
       this.obtenerUsuario();
+      this.obtenerTotal();
       // this.clientService.autenticado = true;
     }
+
+  }
+
+  async obtenerTotal() {
+    await this.obtenerPedidos();
+    this.listaPedidos.forEach(element => {
+      this.total += element.total;
+    });
+
+    this.total = parseFloat(this.total).toFixed(2);
 
   }
 
@@ -32,6 +50,23 @@ export class DashboardComponent implements OnInit {
         },
         err => console.error(err)
       );
+  }
+
+  obtenerPedidos(): Promise<boolean> {
+    return new Promise(
+      resolve => {
+        this.pedidoService.obtenerPedidosCliente(this.idUsuario)
+          .subscribe(
+            res => {
+              this.listaPedidos = res;
+              resolve(true);
+            },
+            err => {
+              console.error(err)
+              resolve(false);
+            }
+          );
+      });
   }
 
 }
