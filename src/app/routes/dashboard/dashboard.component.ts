b561cf318dfbb257e9ddf30c1a6e9688140510b6
dtorresc2 <dtorresc2@miumg.edu.gt>
2020-09-25 21:38:53 -0600

    Agregando dashboard 2

--- src/app/routes/dashboard/dashboard.component.ts
+++ src/app/routes/dashboard/dashboard.component.ts
@@ -14,6 +14,13 @@ export class DashboardComponent implements OnInit {
   listaPedidos: any = [];
   total: any = 0;
 
+  nombreCompleto: any;
+  usuario: any;
+  correo: any;
+  dpi: any;
+
+  ultimoActividad: any;
+
   constructor(
     private clientService: ClientsService,
     private router: Router,
@@ -26,6 +33,7 @@ export class DashboardComponent implements OnInit {
       this.idUsuario = localStorage.getItem('idUsuario');
       this.obtenerUsuario();
       this.obtenerTotal();
+      this.obtenerBitacora();
       // this.clientService.autenticado = true;
     }
 
@@ -46,7 +54,27 @@ export class DashboardComponent implements OnInit {
       .subscribe(
         res => {
           localStorage.setItem('userName', (<any>res).nick);
-          // console.log(res);
+          console.log(res);
+          this.nombreCompleto = (<any>res).nombre + ' ' + (<any>res).apellido;
+          this.usuario = (<any>res).nick;
+          this.dpi = (<any>res).dpi;
+          this.correo = (<any>res).correo;
+        },
+        err => console.error(err)
+      );
+  }
+
+  obtenerBitacora() {
+    this.clientService.obtenerBitacora(this.idUsuario)
+      .subscribe(
+        res => {
+          // localStorage.setItem('userName', (<any>res).nick);
+          // console.log((<any>res[0]).id);
+          // console.log((<any>res[0]).fecha, '-', (<any>res[0]).hora);
+          this.ultimoActividad = (<any>res[0]).fecha + ' ' + (<any>res[0]).hora;
+          // this.nombreCompleto = (<any>res).nombre + ' ' + (<any>res).apellido;
+          // this.usuario = (<any>res).nick;
+          // this.dpi = (<any>res).dpi;
         },
         err => console.error(err)
       );
