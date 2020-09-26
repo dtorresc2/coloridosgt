ecc65616b0a5c556abeda7defa7371515241b2a1
dtorresc2 <dtorresc2@miumg.edu.gt>
2020-09-25 23:04:31 -0600

    Agregando dashboard 4

--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from 'src/app/services/usuarios/users.service';
 import { Router } from '@angular/router';
+import { PedidosService } from 'src/app/services/pedidos/pedidos.service';
+import { FinanzaService } from 'src/app/services/finanzas/finanza.service';
+import { NgbCalendar } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
@@ -12,18 +15,27 @@ export class HomeComponent implements OnInit {
   idUsuario: any;
 
   arregloUsuario: any = [];
+  arregloPedidos: any = [];
   usuario: any;
   correo: any;
   intervalo;
+
+  contadorPedidos: any = 0;
 
   moduloUsuarios: boolean = false;
   moduloPedidos: boolean = false;
   moduloProductos: boolean = false;
   moduloFinanzas: boolean = false;
 
-  ultimaActividad:any;
+  ultimaActividad: any;
 
-  constructor(private usersService: UsersService, private router: Router) { }
+  constructor(
+    private usersService: UsersService,
+    private router: Router,
+    private pedidoService: PedidosService,
+    private finanzaService: FinanzaService,
+    private calendar: NgbCalendar
+  ) { }
 
   ngOnInit(): void {
     this.idUsuario = localStorage.getItem('idUsuario');
@@ -37,26 +49,22 @@ export class HomeComponent implements OnInit {
     }
 
     this.obtenerUsuario();
-
     this.obtenerBitacora();
+    this.obtenerDetallesPedido();
+
+    
+    console.log(this.calendar.getToday());
+    // this.obtenerGanancias();
 
     this.intervalo = setInterval(() => {
       if (localStorage['idUsuario']) {
         this.usersService.obtenerPermisos(localStorage.getItem('idUsuario'))
           .subscribe(
             res => {
-              // console.log((<any>res[0]).bitacora_y_usuario);
               this.moduloUsuarios = (<any>res[0]).bitacora_y_usuario == 1 ? true : false;
               this.moduloPedidos = (<any>res[0]).ventas == 1 ? true : false;
               this.moduloProductos = (<any>res[0]).inventario == 1 ? true : false;
               this.moduloFinanzas = (<any>res[0]).finanzas == 1 ? true : false;
-
-              // this.usersService.moduloUsuarios = this.moduloUsuarios;
-              // this.usersService.moduloPedidos = this.moduloPedidos;
-              // this.usersService.moduloProductos = this.moduloProductos;
-              // this.usersService.moduloFinanzas = this.moduloFinanzas;
-              // this.moduloUsuarios = (<any>res).
-              // console.log(res);
             },
             err => console.error(err)
           );
@@ -68,11 +76,8 @@ export class HomeComponent implements OnInit {
     this.usersService.obtenerUsuarios()
       .subscribe(
         res => {
-          // localStorage.setItem('userName', (<any>res).nick);
-          // console.log(res);
           this.arregloUsuario = res;
           let encontrado = this.arregloUsuario.find(element => element.idusuario = this.idUsuario);
-          // console.log(encontrado.correo);
           this.usuario = encontrado.nombrerol;
           this.correo = encontrado.correo;
         },
@@ -84,7 +89,43 @@ export class HomeComponent implements OnInit {
     this.usersService.obtenerBitacora(localStorage.getItem('idUsuario'))
       .subscribe(
         res => {
-          // console.log(res);
+          this.ultimaActividad = (<any>res[0]).fecha + ' ' + (<any>res[0]).hora;
+        },
+        err => console.error(err)
+      );
+  }
+
+  obtenerGanancias(): Promise<boolean> {
+    return new Promise(
+      resolve => {
+        this.pedidoService.obtenerPedidos()
+          .subscribe(
+            res => {
+              this.arregloPedidos = res;
+              resolve(true);
+            },
+            err => {
+              console.error(err);
+              resolve(false);
+            }
+          );
+      });
+  }
+
+  async obtenerDetallesPedido() {
+    await this.obtenerGanancias();
+
+    this.arregloPedidos.forEach(element => {
+      if (element.idusuario == this.idUsuario) {
+        this.contadorPedidos++;
+      }
+    });
+  }
+
+  obtenerGananciasHoy(){
+    this.finanzaService.obtenerIngresos('','')
+      .subscribe(
+        res => {
           this.ultimaActividad = (<any>res[0]).fecha + ' ' + (<any>res[0]).hora;
         },
         err => console.error(err)
